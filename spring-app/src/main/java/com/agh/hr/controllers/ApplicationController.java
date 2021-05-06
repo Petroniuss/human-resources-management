54b9f3ef7bd463749d444b4b88755ba877d1031d
Patryk Wojtyczek <31375809+Petroniuss@users.noreply.github.com>
2021-05-06 18:50:01 +0200

    Update ApplicationController.java

--- spring-app/src/main/java/com/agh/hr/controllers/ApplicationController.java
+++ spring-app/src/main/java/com/agh/hr/controllers/ApplicationController.java
@@ -8,6 +8,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.io.IOException;
 
 import java.util.List;
 import java.util.stream.Collectors;
